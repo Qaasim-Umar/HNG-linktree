66313e599a964ff61e042b8188186dbd24016a2c
Qaasim Umar <omayr010@gmail.com>
2022-11-01 12:50:20 +0100

    fixed the profile img hover

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React from "react";
 import logo from "./logo.png";
-import mobile from'./mobile.svg';
-import desktop from './desktop.svg'
-import tooltip from './Tooltip.svg'
-import './Header.css'
+import mobile from "./mobile.svg";
+import desktop from "./desktop.svg";
+import tooltip from "./Tooltip.svg";
+import camera from "./camera.svg";
+import "./Header.css";
 
 const Header = () => {
   return (
-     <div className="app">
-       <img src={tooltip} id='tooltip' alt='share-btn'/>
-      <img src={desktop} id='desktop' alt='share-btn'/>
-      <img src={mobile} id='mobile' alt='share-btn'/>
-      <img src={logo} id='profile_img' alt='my-img' />
-      <p><b>Qaasim Umar</b></p>
-    </div>
-  )
-}
+    <div className="app">
+      <img src={tooltip} id="tooltip" alt="share-btn" />
+      <img src={desktop} id="desktop" alt="share-btn" />
+      <img src={mobile} id="mobile" alt="share-btn" />
+      <img src={logo} id="profile_img" alt="my-img" />
 
-export default Header
+      <img src={camera} id="camera" alt="my-img" />
+
+      <p>
+        <b>Qaasim Umar</b>
+      </p>
+    </div>
+  );
+};
+
+export default Header;
